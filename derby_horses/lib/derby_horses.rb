7df41e7bd771ae380904c4ced4944ececa6998be
Anne Edmondson <edmondson.anne@gmail.com>
2020-06-13 01:27:18 +0000

    Automatically backed up by Learn

--- derby_horses/lib/derby_horses.rb
+++
@@ -1,6 +0,0 @@
-girequire "derby_horses/version"
-
-module DerbyHorses
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- derby_horses/lib/derby_horses/version.rb
+++
@@ -1,3 +0,0 @@
-module DerbyHorses
-  VERSION = "0.1.0"
-end
